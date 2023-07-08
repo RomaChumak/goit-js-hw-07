af34ff27b9b2b7585e06478772b0f6f5ab7f92f6
RomaChumak <romachumak355@gmail.com>
2023-07-08 19:08:14 +0300

    2

---
+++ src/js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const imgList = galleryItems.map(({ preview, original, description }) => 
+`<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+
+gallery.insertAdjacentHTML('afterbegin', imgList)
+ 
+gallery.addEventListener("click", onOpenLargeImage)
+function onOpenLargeImage(evt) {
+      evt.preventDefault()
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    }
+    const instanse = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}" alt="${evt.target.description}"
+         width="1300" height="900" data-source="${evt.target.dataset.source}">`,
+        {
+            onShow: handlerEscapeModal,
+            onClose: handlerEscapeModal,
+        }
+    );
+
+    instanse.show();
+    console.log("onOpenLargeImage data", instanse);
+    function handlerEscapeModal(evt) {
+    if (evt.code === "Escape") {
+        instance.close();
+    }
+    console.log(evt.code)
+}
+}
+ 
+
+window.addEventListener("keydown", handlerEscapeModal);
+window.removeEventListener("keydown", handlerEscapeModal);
+
